8f26e54e705bbfcfdedbfb41e4468d8768c26c81
tomifrongi <38133977+tomifrongi@users.noreply.github.com>
2020-04-10 17:46:44 -0300

    Se agrega clase usuario faltante, para no sobrescribir clase con seguridad

---
+++ src/main/java/utn/dds/tpAnual/Usuario.java
@@ -0,0 +1,27 @@
+
+
+/**
+ * @author Daiana
+ * @version 1.0
+ * @created 10-abr.-2020 17:39:46
+ */
+public class Usuario {
+
+	private int cantidadIntentos;
+	private String contrasenia;
+	private LocalDateTime fechaEspera;
+	private String nombre;
+	public UsuarioEntidad m_UsuarioEntidad;
+	public Seguridad_Usuario m_Seguridad_Usuario;
+
+	public Usuario(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public void validarContrasenia(){
+
+	}
+}//end Usuario
