837df46a8885aeb80f6921453ac2ba0a3e2a8898
mdiazpereyra <mariano.diaz.pereyra@gmail.com>
2020-04-27 02:48:07 -0300

    Corregidos CUIT y direccionPostal duplicada

--- src/main/java/utn/dds/tpAnual/proveedor/ProveedorJuridico.java
+++ src/main/java/utn/dds/tpAnual/proveedor/ProveedorJuridico.java
@@ -8,13 +8,12 @@ package utn.dds.tpAnual.proveedor;
  */
 public class ProveedorJuridico extends Proveedor {
 
-	private Long CUIT;
-	private int direccionPostal;
+	private Long cuit;
 	private String razonSocial;
 
-	public ProveedorJuridico(){
-
+	public ProveedorJuridico(int direccionPostal, Long cuit, String razonSocial) {
+		super(direccionPostal);
+		this.cuit = cuit;
+		this.razonSocial = razonSocial;
 	}
-
-
 }
