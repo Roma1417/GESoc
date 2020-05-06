d53af9e57292934ea4b47ef3af31b1ba9e4ba717
rdiaz <07.rdiaz@gmail.com>
2020-05-05 22:14:40 -0300

    Se corrige llamada a constructores

--- src/test/java/utn/dds/tpAnual/compra/OperacionEfectuadaTest.java
+++ src/test/java/utn/dds/tpAnual/compra/OperacionEfectuadaTest.java
@@ -8,16 +8,9 @@ import utn.dds.tpAnual.*;
 
 public class OperacionEfectuadaTest {
 
-	private Egreso egresoTest = new Egreso(null, null, 0, null, null, null, 10, null, null, null, null);
-	
-    @Test
-    public void getTotalDeOperacionEfectuadaSiNoHayDetalles() {
-    	assertTrue(egresoTest.getTotal().equals(0F));
-    }
-
     @Test
     public void getTotalDeOperacionEfectuada() {
-    	Egreso egreso = new Egreso(0, 0, null, null, null);
+    	Egreso egreso = new Egreso(null, null, 0, null, null, null, 0, null, null, null, null);
     	Item itemTest = new Item(null, null);
     	DetalleOperacion d1 = new DetalleOperacion(itemTest, 50F, 3); // 150
     	DetalleOperacion d2 = new DetalleOperacion(itemTest, 10F, 4); // 40
@@ -33,7 +26,7 @@ public class OperacionEfectuadaTest {
     
     @Test
     public void getTotalDeOperacionEfectuadaSiNoHayDetalles() {
-    	Egreso egreso = new Egreso(0, 0, null, null, null);
+    	Egreso egreso = new Egreso(null, null, 0, null, null, null, 0, null, null, null, null);
     	assertTrue(egreso.getTotal().equals(0F));
     }
 }
