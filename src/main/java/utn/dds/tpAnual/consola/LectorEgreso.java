0eaa3d60da68c66852142e9743c7440d4a86f3a6
Mariano Díaz <mariano.diaz.pereyra@gmail.com>
2020-05-27 21:47:59 -0300

    Invente Roman invente (limpiado ejecutar)

--- src/main/java/utn/dds/tpAnual/consola/LectorEgreso.java
+++ src/main/java/utn/dds/tpAnual/consola/LectorEgreso.java
@@ -1,15 +1,8 @@
 package utn.dds.tpAnual.consola;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-import utn.dds.tpAnual.compra.DetalleOperacion;
-import utn.dds.tpAnual.compra.DetallePrecio;
-import utn.dds.tpAnual.compra.Item;
-import utn.dds.tpAnual.compra.Presupuesto;
-
-public class LectorEgreso extends Lector {
+public class LectorEgreso extends Lector{
 	private static Scanner scanner = new Scanner(System.in);
 	private static LectorEgreso instance = new LectorEgreso();
 	
@@ -21,18 +14,7 @@ public class LectorEgreso extends Lector {
 	}
 	
 	@Override
-	public void ejecutar() {
-		Item itemTest = new Item(123L, "itemTest");
-		DetalleOperacion unDetalleOperacion = new DetalleOperacion(itemTest, 10F, 3);
-		DetalleOperacion otroDetalleOperacion = new DetalleOperacion(itemTest, 20F, 4);
-		
-		DetallePrecio unDetallePrecio = new DetallePrecio(unDetalleOperacion, 17F);
-		DetallePrecio otroDetallePrecio = new DetallePrecio(otroDetalleOperacion, 12F);
-		List<DetallePrecio> unosDetallesPrecio = Arrays.asList(unDetallePrecio, otroDetallePrecio);
-		
-		Presupuesto unPresupuesto = new Presupuesto(null, null, 1782, unosDetallesPrecio);
-		
-		System.out.println(unPresupuesto);
+	public void ejecutar(){
+
 	}
-	
 }
