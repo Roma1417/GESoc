62bf5ef8b7c53082965901aa94d450fb0a7ca50c
dmaidana <dmaidana@hexacta.com>
2020-06-15 11:37:22 -0300

    EgresoBuilder, lo termino

--- src/test/java/utn/dds/tpAnual/afip/builders/EgresoBuilder.java
+++ src/test/java/utn/dds/tpAnual/afip/builders/EgresoBuilder.java
@@ -10,10 +10,6 @@ import utn.dds.tpAnual.validador.CriterioCompra;
 
 public class EgresoBuilder {
 	
-	// int codigoOperacion, List<DetalleOperacion> detallesOperacion, LocalDate fechaOperacion, MedioPago medioPago, int cantidadPresupuestosMinimos, CriterioCompra criterioCompra, List<Presupuesto> presupuestos, Proveedor proveedor, List<Usuario> revisores)
-	private Egreso egresoSinPresupuestos = new Egreso(null, null, 542, null, null, null, 2, criterioMenorPrecio, listaPresupuestosVacia, null, revisoresTest);
-	private Egreso egresoCumplidor = new Egreso(null, null, 87, unosDetallesOperacion, null, null, 2, criterioMenorPrecio, unaListaPresupuestos, null, revisoresTest);
-
 	private int codigoOperacion;
     private List<DetalleOperacion> detallesOperacion;
     private int cantidadPresupuestosMinimos;
@@ -25,30 +21,26 @@ public class EgresoBuilder {
         this.codigoOperacion = codigoOperacion;
         return this;
     }
-    
     public EgresoBuilder withDetallesOperacion(List<DetalleOperacion> detallesOperacion){
         this.detallesOperacion = detallesOperacion;
         return this;
     }
-    
     public EgresoBuilder withCantidadPresupuestosMinimos(int cantidadPresupuestosMinimos){
         this.cantidadPresupuestosMinimos = cantidadPresupuestosMinimos;
         return this;
     }
-
-    
-    public EgresoBuilder withdetallesOperacion(List<DetalleOperacion> detallesOperacion){
-        this.detallesOperacion = detallesOperacion;
+    public EgresoBuilder withCriterioCompra(CriterioCompra criterioCompra){
+        this.criterioCompra = criterioCompra;
         return this;
     }
-
-    
-    public EgresoBuilder withdetallesOperacion(List<DetalleOperacion> detallesOperacion){
-        this.detallesOperacion = detallesOperacion;
+    public EgresoBuilder withPresupuestos(List<Presupuesto> presupuestos){
+        this.presupuestos = presupuestos;
         return this;
     }
-
-
+    public EgresoBuilder withRevisores(List<Usuario> revisores){
+        this.revisores = revisores;
+        return this;
+    }
     public Egreso build(){
         return new Egreso(null, null, codigoOperacion, detallesOperacion, null, null, cantidadPresupuestosMinimos, criterioCompra, presupuestos, null, revisores);
     }
