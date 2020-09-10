e48fa4bb12609f0518bb34094cad4c6a979dc42d
rdiazutn <rodrigodiaz@est.frba.utn.edu.ar>
2020-09-10 19:43:10 -0300

    hjghj

--- src/main/java/utn/dds/tpAnual/vinculacion/regla/ReglaVinculacion.java
+++ src/main/java/utn/dds/tpAnual/vinculacion/regla/ReglaVinculacion.java
@@ -1,2 +1,8 @@
-package utn.dds.tpAnual.vinculacion.regla;public class ReglaVinculacion {
+package utn.dds.tpAnual.vinculacion.regla;
+
+import utn.dds.tpAnual.db.entity.transaccion.Egreso;
+import utn.dds.tpAnual.db.entity.transaccion.Ingreso;
+
+public abstract class ReglaVinculacion {
+    public abstract boolean puedeVincularse(Ingreso ingreso, Egreso egreso);
 }
