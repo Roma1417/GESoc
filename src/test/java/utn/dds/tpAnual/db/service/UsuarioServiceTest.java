20c466ba08135cc7be963f7d61470d7769a891b8
rdiazutn <rodrigodiaz@est.frba.utn.edu.ar>
2020-10-07 22:42:17 -0300

    Se corrige cfg de h2

--- src/test/java/utn/dds/tpAnual/db/service/UsuarioServiceTest.java
+++ src/test/java/utn/dds/tpAnual/db/service/UsuarioServiceTest.java
@@ -3,8 +3,11 @@ package utn.dds.tpAnual.db.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import utn.dds.tpAnual.builders.UsuarioBuilder;
 import utn.dds.tpAnual.db.entity.usuario.Usuario;
@@ -14,8 +17,10 @@ import utn.dds.tpAnual.db.service.jpaService.UsuarioService;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 @MockBean(ProgramadorDeTareas.class)
+@DataJpaTest(showSql=false)
+@DirtiesContext
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioServiceTest {
 
     @Autowired
