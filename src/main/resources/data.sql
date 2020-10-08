07a02c42f51f81fb2e2687ab61a231f4058a8085
rdiazutn <rodrigodiaz@est.frba.utn.edu.ar>
2020-10-07 22:47:04 -0300

    fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ INSERT INTO item VALUES(null, 'Descripcion 1', null, null);
 INSERT INTO item VALUES(null, 'Descripcion 2', null, null);
 INSERT INTO item VALUES(null, 'Descripcion 3', null, null);
 
-INSERT INTO dds_tp.usuario (usuario, cantidad_intentos, contrasenia, fecha_espera, nombre)
+INSERT INTO usuario (usuario, cantidad_intentos, contrasenia, fecha_espera, nombre)
 VALUES('dai', 0, '1234567',null,'Daiana');
-INSERT INTO dds_tp.mensaje (asunto, cuerpo, usuario_id)
+INSERT INTO mensaje (asunto, cuerpo, usuario_id)
 VALUES ('Resultado Validación', 'La validación fue exitosa', 1);
-INSERT INTO dds_tp.mensaje (asunto, cuerpo, usuario_id)
+INSERT INTO mensaje (asunto, cuerpo, usuario_id)
 VALUES ('Resultado Validación', 'La validación no fue exitosa', 1);
