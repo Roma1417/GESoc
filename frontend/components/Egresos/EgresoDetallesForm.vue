c3f7d6020ad9d595198b9d7d8fd607022e843892
daimaidana <daimaidana@est.frba.utn.edu.ar>
2020-11-03 00:03:26 -0300

    commit detalles parcial

--- frontend/components/Egresos/EgresoDetallesForm.vue
+++ frontend/components/Egresos/EgresoDetallesForm.vue
@@ -3,26 +3,39 @@
     <v-card-title>
       {{ $t('egresos.detalles') }}
     </v-card-title>
+    <TheAsyncAutocompleteInput
+      v-model="detailAux.item"
+      item-text="descripcion"
+      :get-items-function="$itemService.getItems"
+      :label="$t('item.item')"
+      @submit="addNewDetail"
+    />
     <TheFilterTable
       class="px-4"
       :items="detalles"
       :headers="headers"
       :total="totalList"
     >
+      <template #[`item.descripcion`]="{ }">
+        {{ item.descripcion }}
+      </template>
       <template #[`item.actions`]="{ }">
-        VINCULAR BOTON
-        <v-icon>
-          mdi-link-variant
-        </v-icon>
+        <TheButtonWithTooltip
+          icon="mdi-trash-can"
+          title="Eliminar detalle"
+          @click="deleteDetail"
+        />
       </template>
     </TheFilterTable>
   </v-card>
 </template>
 <script>
 import TheFilterTable from '~/components/General/Tables/TheFilterTable'
+import TheButtonWithTooltip from '~/components/General/Buttons/TheButtonWithTooltip'
 export default {
   components: {
-    TheFilterTable
+    TheFilterTable,
+    TheButtonWithTooltip
   },
   props: {
     detalles: {
@@ -32,6 +45,7 @@ export default {
   },
   data () {
     return {
+      detailAux: {},
       totalList: 5
     }
   },
@@ -42,7 +56,7 @@ export default {
           text: this.$t('items.item'),
           align: 'start',
           sortable: false,
-          value: 'item'
+          value: 'item.descripcion'
         },
         {
           text: this.$t('detalles.cantidad'),
@@ -71,7 +85,14 @@ export default {
     }
   },
   methods: {
+    newEmptyDetail () {
+      return {
+        item: {}
+      }
+    },
+    deleteDetail (detail) {
 
+    }
   }
 }
 </script>
