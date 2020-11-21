a2d05d2566288cdb9aac0043c517d2a8ff03a2ff
Mariano Díaz <mariano.diaz.pereyra@gmail.com>
2020-11-21 02:03:41 -0300

    Pequenios cambios en la form

--- frontend/components/Presupuestos/PresupuestoDetallesForm.vue
+++ frontend/components/Presupuestos/PresupuestoDetallesForm.vue
@@ -3,70 +3,28 @@
     <v-card-title>
       {{ $t('presupuestos.detalles_precio') }}
     </v-card-title>
-    <v-row class="px-5">
-      <TheResponsiveColumn>
-        <TheAsyncAutocompleteInput
-          v-model="detailAux.item"
-          item-text="descripcion"
-          :ignore-items="detalles.map(detalle => detalle.item)"
-          :match-ignore-function="(i1, i2) => i1.id === i2.id"
-          :cache-items="false"
-          :get-items-function="$itemService.getItems"
-          :label="$t('items.item')"
-          @keyup.enter="addNewDetail"
-        />
-      </TheResponsiveColumn>
-      <v-col>
-        <v-alert
-          color="primary"
-          outlined
-          dense
-          type="info"
-        >
-          {{ $t('items.info_ingresar_item') }}
-        </v-alert>
-      </v-col>
-    </v-row>
     <TheFilterTable
       class="px-4"
       :items="detalles"
       :headers="headers"
       :total="totalList"
     >
-      <template #[`item.cantidad`]="{ item }">
-        <TheNumericInput
-          v-model="item.cantidad"
-          :rules="[$rl.required(),$rl.positive()]"
-        />
-      </template>
       <template #[`item.precio`]="{ item }">
         <TheNumericInput
           v-model="item.precio"
-          :rules="[$rl.required(),$rl.positive()]"
+          :rules="[$rl.positive()]"
         />
-      </template>
-      <template #[`item.actions`]="{ item }">
-        <TheButtonWithTooltip
-          icon="mdi-trash-can"
-          title="Eliminar item"
-          @click="deleteDetail(item)"
-        />
-      </template>
-      <template #[`item.total`]="{ item }">
-        ${{ financial(item.cantidad * item.precio) }}
       </template>
     </TheFilterTable>
   </v-card>
 </template>
 <script>
 import TheFilterTable from '~/components/General/Tables/TheFilterTable'
-import TheButtonWithTooltip from '~/components/General/Buttons/TheButtonWithTooltip'
 import { numericMixin } from '~/services/mixins/numericMixin'
 import TheNumericInput from '~/components/General/Inputs/TheNumericInput'
 export default {
   components: {
     TheFilterTable,
-    TheButtonWithTooltip,
     TheNumericInput
   },
   mixins: [numericMixin],
@@ -86,17 +44,16 @@ export default {
     headers () {
       return [
         {
+          text: this.$t('presupuestos.detalles_operacion'),
+          align: 'center',
+          sortable: false,
+          value: 'detalleOperacion.detalleOperacionId'
+        },
+        {
           text: this.$t('items.item'),
           align: 'start',
           sortable: false,
           value: 'item.descripcion'
-        },
-        {
-          text: this.$t('detalles.cantidad'),
-          align: 'right',
-          sortable: false,
-          width: '15%',
-          value: 'cantidad'
         },
         {
           text: this.$t('detalles.precio'),
@@ -104,19 +61,6 @@ export default {
           sortable: false,
           width: '18%',
           value: 'precio'
-        },
-        {
-          text: this.$t('detalles.total'),
-          align: 'right',
-          sortable: false,
-          width: '20%',
-          value: 'total'
-        },
-        {
-          text: this.$t('actions'),
-          value: 'actions',
-          width: '17%',
-          sortable: false
         }
       ]
     }
