fbb029a5f87873077c874ac7c79e207c90291f53
daimaidana <daimaidana@est.frba.utn.edu.ar>
2020-12-09 00:15:22 -0300

    Autocomplete nombre clase.

---
+++ frontend/components/Business/Auditoria/AuditoriaNombreClase.vue
@@ -0,0 +1,31 @@
+<template>
+  <TheAutocompleteInput
+    :items="items"
+    :loading="loading"
+    hide-no-data
+    :label="$t('auditoria.entidad')"
+    v-bind="$attrs"
+    v-on="$listeners"
+  />
+</template>
+<script>
+export default {
+  data: () => ({
+    loading: false,
+    items: []
+  }),
+  mounted () {
+    this.getEntidades()
+  },
+  methods: {
+    getEntidades () {
+      this.loading = true
+      this.$auditoriaService.getEntidades().then((result) => {
+        if (result) {
+          this.items = result
+        }
+      }).finally(() => { this.loading = false })
+    }
+  }
+}
+</script>
